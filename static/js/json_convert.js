352c48030238565b33fb1e47c07a2ac2afbffc3c
Daan Siepelinga <daan-siepelinga@hotmail.com>
2015-10-14 11:19:57 +0200

    Properties added to the disasters, also aligned them with our natural disaster classes, some of the disasters have incorrect names atm

--- static/js/json_convert.js
+++ static/js/json_convert.js
@@ -21,7 +21,7 @@ function getAreas(data)
 {
     var result = data.result;
     var result_l = result.length;
-    var area_instance, areas, areas_l, areas_append, count = 0;
+    var area_instance, areas, areas_l, areas_append, types, types_l, count = 0;
     
     $("#result").html("<ul></ul>");
 
@@ -34,11 +34,34 @@ function getAreas(data)
         {
             areas_append = "";
         
-            $("#result ul").append("<li>:" + convertSpecialChars(result[i].name) + " rdf:type owl:NamedIndividual ;</li>");
+            $("#result ul").append("<li>:" + convertSpecialCharsDisaster(result[i]["name"]) + " rdf:type owl:NamedIndividual ;</li>");
+            
+            if (result[i]["/time/event/start_date"])
+                $("#result ul").append("<li>:start_date \"" + result[i]["/time/event/start_date"] + "\"^^xsd:string ;</li>");
+                
+            if (result[i]["/time/event/end_date"])
+                $("#result ul").append("<li>:end_date \"" + result[i]["/time/event/end_date"] + "\"^^xsd:string ;</li>");
+                
+            if (result[i]["damage"])
+                $("#result ul").append("<li>:damage \"" + result[i]["damage"] + "\"^^xsd:integer ;</li>");
+                
+            if (result[i]["fatalities"])
+                $("#result ul").append("<li>:fatalities \"" + result[i]["fatalities"] + "\"^^xsd:integer ;</li>");
+                
+            if (result[i]["injuries"])
+                $("#result ul").append("<li>:injuries \"" + result[i]["injuries"] + "\"^^xsd:integer ;</li>");
+
+            types = result[i]["type_of_disaster"];
+            types_l = types.length;
+            for (var k = 0; k < types_l; k++)
+            {
+                console.log(types[k]);
+                $("#result ul").append("<li>:disaster_type \"" + types[k] + "\"^^xsd:string ;"); 
+            }
             
             for (var j = 0; j < areas_l; j++)
             {
-                area_instance = ":" + convertSpecialChars(areas[j]);
+                area_instance = ":" + convertSpecialCharsAreas(areas[j]);
                 
                 if (j == areas_l - 1)
                     $("#result ul").append("<li>:hit " + area_instance + " .</li>");
@@ -55,9 +78,16 @@ function getAreas(data)
     //$("#result").append(count);
 }
 
-/* Removing the characters from the imported data, while ensuring it remains dbpedia compatible */
-function convertSpecialChars(string)
+/* Removing the characters from the imported area data, while ensuring it remains dbpedia compatible */
+function convertSpecialCharsAreas(string)
 {
     /* Replace spaces with '_', apostrophe and brackets+content with nothing and remove slashes/comma's/– with everything after them */
     return string.replace(/ /g, "_").replace(/\u0027/g, '').replace(/\((.*)\)/g, '').replace(/[\/,–](.*)/, '');
+}
+
+/* Removing the characters from the imported disaster data, while ensuring it remains dbpedia compatible */
+function convertSpecialCharsDisaster(string)
+{
+    /* Replace spaces with '_', apostrophe/comma/brackets+content with nothing and remove slashes/– with everything after them */
+    return string.replace(/ /g, "_").replace(/[,\u0027]/g, '').replace(/\((.*)\)/g, '').replace(/[\/–](.*)/, '');
 }
